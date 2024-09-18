3c75baf3609809850923eef082c2cb94ec1aee98
zapfe@secondred.de <zapfe@secondred.de>
2024-09-18 08:44:14 +0200

    current

--- api/web/index.php
+++ api/web/index.php
@@ -4,9 +4,9 @@ session_start();
 // Define path constants
 use src\App;
 
-define('BASE_PATH', dirname(__DIR__) . '\\');
-define('VENDOR_PATH', dirname(__DIR__) . '\\vendor');
-define('ASSET_PATH', dirname(__DIR__) . '\\web\\assets');
+define('BASE_PATH', dirname(__DIR__) . '/');
+define('VENDOR_PATH', dirname(__DIR__) . '/vendor');
+define('ASSET_PATH', dirname(__DIR__) . '/web/assets');
 
 // include all classes
 spl_autoload_register(function ($class_name) {
