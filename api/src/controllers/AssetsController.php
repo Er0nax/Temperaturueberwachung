3133d472412ad2eed781d1e1f81f6bbea1a09186
zapfe@secondred.de <zapfe@secondred.de>
2024-10-16 10:11:50 +0200

    updated asset controller

--- api/src/controllers/AssetsController.php
+++ api/src/controllers/AssetsController.php
@@ -3,23 +3,21 @@
 namespace src\controllers;
 
 use JetBrains\PhpStorm\NoReturn;
-use src\components\Entry;
 use src\helpers\FileHelper;
 use src\helpers\ResultHelper;
-use src\helpers\UserHelper;
 
 /**
  * @author Tim Zapfe
  * @copyright Tim Zapfe
  * @date 15.10.2024
  */
-class AssetController extends BaseController
+class AssetsController extends BaseController
 {
-    private string $name = 'default';
-    private string $extension = 'png';
-    private string $type = 'general';
-    private ?string $height = null;
-    private ?string $width = null;
+    private ?string $name;
+    private ?string $extension;
+    private ?string $type;
+    private ?string $height;
+    private ?string $width;
 
     /**
      * Sets the information about an asset.
@@ -28,30 +26,22 @@ class AssetController extends BaseController
      * @copyright Tim Zapfe
      * @date 15.10.2024
      */
-    private function setInfo(): void
+    private function requireInfo(): void
     {
-        $this->name = $this->getParam(0, 'name', 'default');
-        $this->extension = $this->getParam(1, 'extension', 'png');
-        $this->type = $this->getParam(2, 'type', 'general');
-        $this->height = $this->getParam(3, 'height');
-        $this->width = $this->getParam(4, 'width');
+        $this->name = $this->getParam(0, 'src', 'default', true);
+        $this->type = $this->getParam(1, 'type', 'general', true);
+        $this->height = $this->getParam(2, 'height', null, true);
+        $this->width = $this->getParam(3, 'width', null, true);
 
         if (empty($this->params)) {
             ResultHelper::render([
                 'info' => 'Please provide the following params:',
                 'params' => [
                     'name' => 'The image full name without extension.',
-                    'extension' => 'The image extension (like png, jpg, jpeg).',
                     'type (optional)' => 'The type of the image (like general, background, cover).',
                     'width (optional)' => 'The width of the displayed image in pixels.',
                     'height (optional)' => 'The height of the displayed image in pixels.',
                 ]
-            ], 500, $this->defaultConfig);
-        }
-
-        if (empty($this->name) || empty($this->extension)) {
-            ResultHelper::render([
-                'info' => 'Please provide the full name and the extension of the asset.',
             ], 500, $this->defaultConfig);
         }
     }
@@ -65,6 +55,26 @@ class AssetController extends BaseController
      */
     private function getName(): string
     {
+        // get the info for the image
+        $info = pathinfo($this->name);
+
+        if (empty($info['filename']) || empty($info['extension'])) {
+            ResultHelper::render([
+                'info' => 'Please provide a valid name with the extension for the asset.'
+            ], 500, $this->defaultConfig);
+        }
+
+        if (!in_array($info['extension'], ['jpg', 'jpeg', 'png', 'gif', 'mp4', 'zip', 'rar', 'pdf', 'mp4', 'mp3', 'webp'])) {
+            ResultHelper::render([
+                'info' => 'Your asset extension is not valid.'
+            ], 500, $this->defaultConfig);
+        }
+
+        // set values
+        $this->name = $info['filename'];
+        $this->extension = $info['extension'];
+
+        // update filename
         return $this->name . '.' . $this->extension;
     }
 
@@ -78,11 +88,7 @@ class AssetController extends BaseController
     #[NoReturn] public function actionImage(): void
     {
         // set information
-        $this->setInfo();
-
-        if (!in_array($this->extension, ['png', 'jpg', 'jpeg', 'gif'])) {
-            $this->extension = 'png';
-        }
+        $this->requireInfo();
 
         // get src
         $src = FileHelper::getImage($this->getName(), $this->type, ['height' => $this->height, 'width' => $this->width]);
