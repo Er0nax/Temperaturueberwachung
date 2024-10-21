95f1fcff79de3547a82a2a520e6b74a796104a8f
Tim Zapfe <zapfe@secondred.de>
2024-10-21 21:09:47 +0200

    added custom dashboard

---
+++ webapp/alex/index.php
@@ -0,0 +1,69 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Temperaturüberwachung</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <link href="assets/css/main.css" rel="stylesheet">
+</head>
+<body>
+
+
+<div class="row justify-content-center text-center">
+    <p class="text-center"><b>Temperaturüberwachung WEB</b></p>
+
+    <div class="card-border">
+        <div><h5 class="mb-2">Server: </h5>
+            <h6 class="text-body-tertiary">Temperatur</h6>
+        </div>
+
+        <div class="d-flex justify-content-left pt-3 flex-1">
+            <canvas style="position: absolute; left: 0; top: 0; width: 144px; height: 144px; user-select: none; padding: 0; margin: 0; border-width: 0px;" data-zr-dom-id="zr_0" width="668"
+                    height="180"></canvas>
+            <div
+            <div class="circle-container">
+                <div class="circle-temp">
+                </div>
+                <div class="my-circle">
+                    <h1 class="Temp_Text"> 37 °C </h1>
+                </div>
+                <div class="circle-container-bottom">
+                </div>
+            </div>
+        </div>
+
+        <div class="mt-3 Max_Min_Temp">
+            <div class="d-flex align-items-center mb-2">
+                <div class="bullet-item bg-primary me-2">
+                </div>
+                <h6 class="text-body fw-semibold flex-1 mb-0">Maximal Temperatur: &nbsp</h6>
+                <h6 class="text-body fw-semibold mb-0">30°C</h6>
+            </div>
+            <div class="d-flex align-items-center">
+                <div class="bullet-item bg-primary-subtle me-2">
+                </div>
+                <h6 class="text-body fw-semibold flex-1 mb-0">Minimal Temperatur: &nbsp</h6>
+                <h6 class="text-body fw-semibold mb-0">0°C</h6>
+            </div>
+        </div>
+    </div>
+
+    <script>
+
+        const circles = document.querySelectorAll('.circle-temp');
+        const temperature = 37; // Assuming temperature in Celsius
+
+        function rotateCircle() {
+            circles.forEach((circle) => {
+                const angle = temperature * 1.8; // Adjust factor for desired rotation range
+                circle.style.transform = `rotate(${angle}deg)`;
+            });
+            requestAnimationFrame(rotateCircle);
+        }
+
+        rotateCircle();
+
+    </script>
+</body>
+</html>
