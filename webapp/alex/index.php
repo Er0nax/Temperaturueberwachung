4c3b01043f5492e832dd3c1e31573dcbf8b36611
Alex <alex.glaetzer@gmail.com>
2024-10-23 11:37:34 +0200

    Templog

--- webapp/alex/index.php
+++ webapp/alex/index.php
@@ -6,6 +6,8 @@
     <title>Temperaturüberwachung</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     <link href="assets/css/main.css" rel="stylesheet">
+    <link href="https://getbootstrap.com/docs/5.3/assets/css/docs.css" rel="stylesheet">
+     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
 </head>
 <body>
 
@@ -16,48 +18,97 @@
     <p class="text-center mt-3"><b> <h1> Temperaturüberwachung WEB </h1> </b></p>
 
     <div class="card-border mt-4">
-        <div><h5 class="mb-2">Server: </h5>
-            <h6 class="text-body-tertiary">Temperatur</h6>
-        </div>
+      <div><h5 class="mb-2">Server: </h5>
+          <h6 class="text-body-tertiary">Temperatur</h6>
+      </div>
 
-        <div class="d-flex justify-content-left pt-3 flex-1">
-            <canvas style="position: absolute; left: 0; top: 0; width: 144px; height: 144px; user-select: none; padding: 0; margin: 0; border-width: 0px;" data-zr-dom-id="zr_0" width="668"
-                    height="180"></canvas>
-            <div
-            <div class="circle-container">
-                <div class="circle-temp">
-                </div>
-                <div class="my-circle">
-                    <h1 class="Temp_Text"> 37 °C </h1>
-                </div>
-                <div class="circle-container-bottom">
-                </div>
-            </div>
-        </div>
+      <div class="d-flex justify-content-left pt-3 flex-1">
+          <canvas style="position: absolute; left: 0; top: 0; width: 144px; height: 144px; user-select: none; padding: 0; margin: 0; border-width: 0px;" data-zr-dom-id="zr_0" width="668"
+                  height="180"></canvas>
+          <div
+          <div class="circle-container">
+              <div class="circle-temp">
+              </div>
+              <div class="my-circle">
+                  <h1 class="Temp_Text"> 37 °C </h1>
+              </div>
+              <div class="circle-container-bottom">
+              </div>
+          </div>
+      </div>
 
-        <div class="mt-3 Max_Min_Temp">
-            <div class="d-flex align-items-center mb-2">
-                <div class="bullet-item bg-primary me-2">
-                </div>
-                <h6 class="text-body fw-semibold flex-1 mb-0">Max. Temp.: &nbsp</h6>
-                <h6 class="text-body fw-semibold mb-0">30°C</h6>
-            </div>
-            <div class="d-flex align-items-center mb-2">
-                <div class="bullet-item bg-primary-subtle me-2">
-                </div>
-                <h6 class="text-body fw-semibold flex-1 mb-0">Min. Temp.: &nbsp</h6>
-                <h6 class="text-body fw-semibold mb-0">0°C</h6>
-            </div>
-            <div class="d-flex align-items-center">
-                <div class="bullet-item bg-primary-subtle me-2">
-                </div>
-                <h6 class="text-body fw-semibold flex-1 mb-0">Durchs. Temp.: &nbsp</h6>
-                <h6 class="text-body fw-semibold mb-0">10°C</h6>
-            </div>
+      <div class="mt-3 Max_Min_Temp">
+          <div class="d-flex align-items-center mb-2">
+              <div class="bullet-item bg-primary me-2">
+              </div>
+              <h6 class="text-body fw-semibold flex-1 mb-0">Max. Temp.: &nbsp</h6>
+              <h6 class="text-body fw-semibold mb-0">30°C</h6>
+          </div>
+          <div class="d-flex align-items-center mb-2">
+              <div class="bullet-item bg-primary-subtle me-2">
+              </div>
+              <h6 class="text-body fw-semibold flex-1 mb-0">Min. Temp.: &nbsp</h6>
+              <h6 class="text-body fw-semibold mb-0">0°C</h6>
+          </div>
+          <div class="d-flex align-items-center">
+              <div class="bullet-item bg-primary-subtle me-2">
+              </div>
+              <h6 class="text-body fw-semibold flex-1 mb-0">Durchs. Temp.: &nbsp</h6>
+              <h6 class="text-body fw-semibold mb-0">10°C</h6>
+          </div>
+      </div>
+
+      <button class="btn btn-primary card_User_Temp_history" autofocus type="button" data-bs-toggle="collapse" data-bs-target="#collapseWidthExample" aria-expanded="false" aria-controls="collapseWidthExample">
+          Temperatur Log
+        </button>
+      <div style="min-height: 120px;">
+        <div class="collapse collapse-horizontal" id="collapseWidthExample">
+          <div class="card card-body" style="width: 300px; top: 100px;">
+
+            <table id="temperatureTable">
+                <thead>
+                    <tr>
+                        <th>Date</th>
+                        <th>Time</th>
+                        <th>Temperature</th>
+                        </tr>
+                </thead>
+                <tbody>
+                  <script>
+                  // JavaScript code to create the table dynamically
+                  // JavaScript mit JSON-Integration
+                    function createTemperatureTable(jsonData) {
+                      const table = document.getElementById("temperatureTable");
+                      const tbody = table.querySelector("tbody");
+
+                      jsonData.forEach((row) => {
+                          const tr = document.createElement("tr");
+                          for (const value of Object.values(row)) {
+                              const td = document.createElement("td");
+                              td.textContent = value;
+                              tr.appendChild(td);
+                          }
+                          tbody.appendChild(tr);
+                      });
+                    }
+
+                    // Beispiel-JSON-Daten
+                    const temperatureData = [
+                      { date: "2023-10-23", time: "12:00", temperature: 22.5 },
+                      { date: "2023-10-23", time: "13:00", temperature: 23.0 },
+                      // ... Weitere Einträge
+                    ];
+
+                    // Funktion aufrufen, um die Tabelle zu erstellen
+                    createTemperatureTable(temperatureData);
+                  </script>
+                </tbody>
+            </table>
+
+          </div>
         </div>
-        <div>
-          <a href="temp_history.php" type="button" data-mdb-button-init data-mdb-ripple-init class="btn btn-primary btn-block mb-4 card_User_Temp_history"> <h4> Temperatur Log </h4> </a>
-        </div>
+      </div>
+
     </div>
     <div class="card-border mt-4">
         <div><h5 class="mb-2">Server: </h5>
